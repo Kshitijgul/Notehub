ada351758ccb002eecbb65ad47d7c7289df3521d
Kshitij <kshitijguladhe2002@gmail.com>
2026-04-03 22:17:19 +0530

    Fix the bug in the Backedn server

--- Backend/server.js
+++ Backend/server.js
@@ -199,7 +199,8 @@ pollGithubTree();
 // Production static serving
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../dist')));
-  app.get('*', (req, res) => {
+  
+  app.use((req, res) => {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
   });
 }
